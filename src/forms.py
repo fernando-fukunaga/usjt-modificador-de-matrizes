c8f68b1e99cb6978016d0c4694004d980f3ed87f
fernando-fukunaga <fernandofukunaga14@gmail.com>
2024-05-26 15:51:18 -0300

    Atualizando frontend para mostrar novas opções de efeito

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
-from wtforms import SelectField, SubmitField, FileField
+from wtforms import SelectField, SubmitField, FileField, IntegerField, BooleanField
+from wtforms.validators import InputRequired
 
 
 class MainForm(FlaskForm):
@@ -10,4 +11,20 @@ class MainForm(FlaskForm):
                                                   ("green", "Verde"),
                                                   ("blue", "Azul"),
                                                   ("inverted", "RGB Invertido")])
+    rotation = SelectField(label="rotation", choices=[("90degrees", "90 graus"),
+                                                      ("180degrees", "180 graus"),
+                                                      ("270degrees", "270 graus")])
+    translation = SelectField(label="translation", choices=[("up", "Para cima"),
+                                                            ("down", "Para baixo"),
+                                                            ("left", "Para esquerda"),
+                                                            ("right", "Para direita")])
+    translation_pixels = IntegerField(label="translationpixels", validators=[InputRequired()])
+    scale = SelectField(label="scale", choices=[("25percent", "25%"),
+                                                ("50percent", "50%"),
+                                                ("75percent", "75%"),
+                                                ("150percent", "150%"),
+                                                ("200percent", "200%")])
+    edge_highlighting = BooleanField(label="edge_highlighting")
+    mirroring = BooleanField(label="mirroring")
+    homography = BooleanField(label="homography")
     submit = SubmitField("Modificar")
