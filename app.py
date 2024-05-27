cfe6a431622f6518837d291a58e0a543bf07d7c1
fernando-fukunaga <fernandofukunaga14@gmail.com>
2024-05-27 15:57:06 -0300

    Adicionado efeito de rotação, implementando translação

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, send_from_directory, request, redirect
 from flask_wtf.csrf import CSRFProtect
 from src.forms import MainForm
 import cv2
+import numpy as np
 
 SECRET_KEY = os.urandom(32)
 IMAGES_PATH = os.path.dirname(os.path.abspath(__file__)) + '/images'
@@ -34,22 +35,38 @@ def image(filename):
 def modify():
     form = MainForm(request.form)
     effect = form.effect.data
+    rotation = form.rotation.data
+    translation = form.translation.data
+    translation_pixels = form.translation_pixels.data
+    scale = form.scale.data
+    edge_highlighting = form.edge_highlighting.data
+    mirroring = form.mirroring.data
+    homography = form.homography.data
     image = request.files["file"]
     images_path = IMAGES_PATH
     timestamp = time.time()
     image.save(f'{images_path}/imagem-{timestamp}-base.jpg')
     image_to_modify = cv2.imread(f'{images_path}/imagem-{timestamp}-base.jpg')
 
-    if effect == "blackwhite":
-        modify_to_black_and_white(image_to_modify, timestamp)
-    elif effect == "red":
-        modify_to_red(image_to_modify, timestamp)
-    elif effect == "green":
-        modify_to_green(image_to_modify, timestamp)
-    elif effect == "blue":
-        modify_to_blue(image_to_modify, timestamp)
-    elif effect == "inverted":
-        modify_to_inverted(image_to_modify, timestamp)
+    if effect != "no_effect":
+        apply_color_effect(image_to_modify, timestamp, effect)
+        image_being_modified = cv2.imread(f'{images_path}/imagem-{timestamp}-modified.jpg')
+    else:
+        image_being_modified = image_to_modify
+
+    if rotation == "90_degrees":
+        rotate_90_degrees(image_being_modified, timestamp)
+        image_being_modified = cv2.imread(f'{images_path}/imagem-{timestamp}-modified.jpg')
+    elif rotation == "180_degrees":
+        rotate_180_degrees(image_being_modified, timestamp)
+        image_being_modified = cv2.imread(f'{images_path}/imagem-{timestamp}-modified.jpg')
+    elif rotation == "270_degrees":
+        rotate_270_degrees(image_being_modified, timestamp)
+        image_being_modified = cv2.imread(f'{images_path}/imagem-{timestamp}-modified.jpg')
+    elif rotation == "no_rotation":
+        pass
+
+    translate_up(image_being_modified, timestamp, translation_pixels)
 
     base_file = f'imagem-{timestamp}-base.jpg'
     modified_file = f'imagem-{timestamp}-modified.jpg'
@@ -58,44 +75,81 @@ def modify():
                             modified_file=modified_file))
 
 
-def modify_to_black_and_white(image, image_timestamp: float) -> None:
+def apply_color_effect(image, image_timestamp: float, effect: str) -> None:
     for y in range(0, image.shape[0]):
         for x in range(0, image.shape[1]):
             (blue, green, red) = image[y, x]
-            image[y, x] = (red, red, red)
+            if effect == "black_white":
+                image[y, x] = (red, red, red)
+            elif effect == "red":
+                image[y, x] = (0, 0, red)
+            elif effect == "green":
+                image[y, x] = (0, green, 0)
+            elif effect == "blue":
+                image[y, x] = (blue, 0, 0)
+            elif effect == "inverted":
+                image[y, x] = (red, green, blue)
     cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', image)
 
 
-def modify_to_red(image, image_timestamp: float) -> None:
-    for y in range(0, image.shape[0]):
-        for x in range(0, image.shape[1]):
-            (blue, green, red) = image[y, x]
-            image[y, x] = (0, 0, red)
-    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', image)
+def rotate_90_degrees(image, image_timestamp: float) -> None:
+    # Obtém a altura e largura da imagem
+    height, width = image.shape[:2]
+
+    # Cria uma nova imagem com as dimensões invertidas para a rotação de 90 graus
+    rotated_image = np.zeros((width, height, 3), dtype=np.uint8)
+
+    # Percorre a imagem original e rotaciona cada pixel manualmente
+    for y in range(height):
+        for x in range(width):
+            rotated_image[x, height - y - 1] = image[y, x]
+
+    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', rotated_image)
 
 
-def modify_to_blue(image, image_timestamp: float) -> None:
-    for y in range(0, image.shape[0]):
-        for x in range(0, image.shape[1]):
-            (blue, green, red) = image[y, x]
-            image[y, x] = (blue, 0, 0)
-    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', image)
+def rotate_180_degrees(image, image_timestamp: float) -> None:
+    # Obtém a altura e largura da imagem
+    height, width = image.shape[:2]
+
+    # Cria uma nova imagem com as mesmas dimensões para a rotação de 180 graus
+    rotated_image = np.zeros((height, width, 3), dtype=np.uint8)
+
+    # Percorre a imagem original e rotaciona cada pixel manualmente
+    for y in range(height):
+        for x in range(width):
+            rotated_image[height - y - 1, width - x - 1] = image[y, x]
+
+    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', rotated_image)
 
 
-def modify_to_green(image, image_timestamp: float) -> None:
-    for y in range(0, image.shape[0]):
-        for x in range(0, image.shape[1]):
-            (blue, green, red) = image[y, x]
-            image[y, x] = (0, green, 0)
-    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', image)
+def rotate_270_degrees(image, image_timestamp: float) -> None:
+    # Obtém a altura e largura da imagem
+    height, width = image.shape[:2]
+
+    # Cria uma nova imagem com as dimensões invertidas para a rotação de 270 graus
+    rotated_image = np.zeros((width, height, 3), dtype=np.uint8)
+
+    # Percorre a imagem original e rotaciona cada pixel manualmente
+    for y in range(height):
+        for x in range(width):
+            rotated_image[width - x - 1, y] = image[y, x]
+
+    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', rotated_image)
 
 
-def modify_to_inverted(image, image_timestamp: float) -> None:
-    for y in range(0, image.shape[0]):
-        for x in range(0, image.shape[1]):
-            (blue, green, red) = image[y, x]
-            image[y, x] = (red, green, blue)
-    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', image)
+def translate_up(image, image_timestamp: float, pixels: int) -> None:
+    # Obtém a altura e largura da imagem
+    height, width = image.shape[:2]
+
+    # Cria uma nova imagem preenchida com zeros (preta)
+    translated_image = np.zeros_like(image)
+
+    # Calcula a nova posição dos pixels após a translação para cima
+    for y in range(pixels, height):
+        for x in range(width):
+            translated_image[y - pixels, x] = image[y, x]
+
+    cv2.imwrite(f'{IMAGES_PATH}/imagem-{image_timestamp}-modified.jpg', translated_image)
 
 
 if __name__ == "__main__":
--- src/forms.py
+++ src/forms.py
@@ -6,24 +6,27 @@ from wtforms.validators import InputRequired
 
 class MainForm(FlaskForm):
     image = FileField(validators=[FileAllowed([".jpg", ".jpeg"])])
-    effect = SelectField(label="effect", choices=[("blackwhite", "Preto e Branco"),
+    effect = SelectField(label="effect", choices=[("no_effect", "Sem efeito"),
+                                                  ("black_white", "Preto e Branco"),
                                                   ("red", "Vermelho"),
                                                   ("green", "Verde"),
                                                   ("blue", "Azul"),
                                                   ("inverted", "RGB Invertido")])
-    rotation = SelectField(label="rotation", choices=[("90degrees", "90 graus"),
-                                                      ("180degrees", "180 graus"),
-                                                      ("270degrees", "270 graus")])
+    rotation = SelectField(label="rotation", choices=[("no_rotation", "Não rotacionar"),
+                                                      ("90_degrees", "90 graus"),
+                                                      ("180_degrees", "180 graus"),
+                                                      ("270_degrees", "270 graus")])
     translation = SelectField(label="translation", choices=[("up", "Para cima"),
                                                             ("down", "Para baixo"),
                                                             ("left", "Para esquerda"),
                                                             ("right", "Para direita")])
     translation_pixels = IntegerField(label="translationpixels", validators=[InputRequired()])
-    scale = SelectField(label="scale", choices=[("25percent", "25%"),
-                                                ("50percent", "50%"),
-                                                ("75percent", "75%"),
-                                                ("150percent", "150%"),
-                                                ("200percent", "200%")])
+    scale = SelectField(label="scale", choices=[("100_percent", "100% (não alterar)"),
+                                                ("25_percent", "25%"),
+                                                ("50_percent", "50%"),
+                                                ("75_percent", "75%"),
+                                                ("150_percent", "150%"),
+                                                ("200_percent", "200%")])
     edge_highlighting = BooleanField(label="edge_highlighting")
     mirroring = BooleanField(label="mirroring")
     homography = BooleanField(label="homography")
